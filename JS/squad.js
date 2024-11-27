d6aaa95b83b83a24625cc64a082d0422d8f639a1
MohamedHamouch <med10hamouch@gmail.com>
2024-11-27 14:59:58 +0100

    pop up toggle active

--- JS/squad.js
+++ JS/squad.js
@@ -1,14 +1,22 @@
-async function fetchPlayers(){
+async function fetchPlayers() {
     const response = await fetch('../assets/players.json');
     const data = await response.json();
     const players = data.players
     console.log(players);
-    localStorage.setItem('players',JSON.stringify(players))
+    localStorage.setItem('players', JSON.stringify(players))
 }
 fetchPlayers();
 
-const positions = document.querySelectorAll('.player');
+const positions = document.querySelectorAll('#squad .player');
+const popUp = document.querySelector('#popup')
 console.log(positions)
 positions.forEach(position => {
-    position.addEventListener('click',()=> console.log(position))
+    position.addEventListener('click', () => {
+        popUp.classList.toggle('hidden')
+    });
 })
+
+const hidePopUpBtn = document.querySelector('#hide-popup-btn')
+hidePopUpBtn.addEventListener('click', () =>{
+    popUp.classList.toggle('hidden')
+});
