3b32b2a3032bef253f4f15b02d41fce446afca70
MohamedHamouch <med10hamouch@gmail.com>
2024-11-28 20:52:47 +0100

    filter pop up by position

--- JS/squad.js
+++ JS/squad.js
@@ -5,6 +5,10 @@ async function fetchPlayers() {
     console.log(players);
     localStorage.setItem('playersData', JSON.stringify(players));
 }
+window.onload = function () {
+    fetchPlayers();
+};
+
 
 const playersData = JSON.parse(localStorage.getItem('playersData'));
 const positions = document.querySelectorAll('#squad .holder-card');
@@ -12,28 +16,19 @@ const hidePopUpBtn = document.querySelector('#hide-popup-btn');
 const popUp = document.querySelector('#popup');
 const popUpList = document.querySelector('#popup-list');
 
-// console.log(playersData);
 
-window.onload = function () {
-    fetchPlayers();
-};
-
-
-
-let selectedPosition;
-positions.forEach(position => {
-    position.addEventListener('click', () => {
+let selectedPlace;
+positions.forEach(positionPlace => {
+    positionPlace.addEventListener('click', () => {
         popUp.classList.toggle('hidden');
-        showPopUpList(playersData);
-        selectedPosition = position;
+        selectedPlace = positionPlace;
+        filterPosition(selectedPlace);
     });
 });
 
 hidePopUpBtn.addEventListener('click', () => {
     popUp.classList.toggle('hidden');
 });
-
-
 
 function showPopUpList(data) {
     popUpList.innerHTML = "";
@@ -58,12 +53,12 @@ function showPopUpList(data) {
         `
         newCard.addEventListener('click', () => {
             popUp.classList.toggle('hidden');
-            selectedPosition.innerHTML = "";
-            selectedPosition.classList.remove('holder-card');
-            selectedPosition.classList.add('gold-card');
+            selectedPlace.innerHTML = "";
+            selectedPlace.classList.remove('holder-card');
+            selectedPlace.classList.add('gold-card');
 
-            selectedPosition.innerHTML = 
-            ` <div class="player-card flex flex-col items-center justify-between h-[80%]">
+            selectedPlace.innerHTML =
+                ` <div class="player-card flex flex-col items-center justify-between h-[80%]">
                     <img src="${player.photo}" alt="${player.name}" class="w-[55%]">
                     <div class="flex flex-col gap-1">
                         <p class="text-sm">${player.name}</p>
@@ -77,12 +72,17 @@ function showPopUpList(data) {
 
                 </div>
             `
-            
+
         })
         popUpList.appendChild(newCard);
     });
 }
 
-
+function filterPosition(positionPlace) {
+    let positionName = positionPlace.getAttribute('data-position');
+    
+    const filteredPlayers = playersData.filter(player => player.position === positionName);
+    showPopUpList(filteredPlayers);
+}
 
 // localStorage.clear();
