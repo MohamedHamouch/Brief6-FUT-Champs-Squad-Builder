fdb2bd8f08701bb3f85dd2bea0450427feeac1c4
MohamedHamouch <med10hamouch@gmail.com>
2024-11-30 01:54:52 +0100

    popup redesign + dynamic stats for gk

--- JS/squad.js
+++ JS/squad.js
@@ -34,66 +34,93 @@ function showPopUpList(data) {
     popUpList.innerHTML = "";
     data.forEach(player => {
 
+
+        let attValue1, attValue2, attValue3, attValue4, attValue5, attValue6;
+        let attName1, attName2, attName3, attName4, attName5, attName6;
+        if (player.position != "GK") {
+            attValue1 = "pace";
+            attValue2 = "shooting";
+            attValue3 = "passing";
+            attValue4 = "dribbling";
+            attValue5 = "defending";
+            attValue6 = "physical";
+
+            attName1 = "PAC";
+            attName2 = "SHO";
+            attName3 = "PAS";
+            attName4 = "DRI";
+            attName5 = "DEF";
+            attName6 = "PHY";
+        } else {
+            attValue1 = "diving";
+            attValue2 = "handling";
+            attValue3 = "kicking";
+            attValue4 = "reflexes";
+            attValue5 = "positioning";
+            attValue6 = "speed";
+
+            attName1 = "DIV";
+            attName2 = "HAN";
+            attName3 = "KIC";
+            attName4 = "REF";
+            attName5 = "POS";
+            attName6 = "SPE";
+        }
+        
         const newCard = document.createElement('button');
-        newCard.className = 'player gold-card w-[12.5rem] h-[18.75rem]'
+        newCard.className = 'text-center flex flex-col justify-center player gold-card h-[13.75rem] w-[10rem] items-center'
         newCard.innerHTML =
-            `      
-            <div class="player-card flex flex-col items-center justify-between h-[75%]">
-                <img src="${player.photo}" alt="${player.name}" class="w-[70%]">
-                    <div class="flex flex-col gap-3">
-                        <p class="text-base">${player.name}</p>
-                        <div class="flex justify-center gap-2">
-                            <p>${player.position}</p>
-                            <p class="rating">${player.rating}</p>
-                            <img src="${player.flag}" alt="" class="h-7">
-                            <img src="${player.logo}" alt="" class="h-7">
-                        </div>
-                    </div>
-             </div>
-        `
+            `<div class="flex w-[90%]">
+        <div class="flex flex-col items-center pt-4">
+            <span class="font-extrabold text-base leading-4">${player.rating}</span>
+            <span class="font-bold text-xs">${player.position}</span>
+            <div class="flex flex-col justify-center items-center gap-1 w-5">
+                <img src="${player.flag}" alt="">
+                <img src="${player.logo}" alt="">
+            </div>
+        </div>
+        <img src="${player.photo}" alt="" class="w-[80%]">
+    </div>
+    <p class="font-bold text-[1rem]">${player.name}</p>
+    <div class="flex gap-1">
+        <div class="flex flex-col justify-center items-center">
+          <span class="text-[.6rem] font-medium">${attName1}</span>
+          <span class="text-[.63rem] font-bold ">${player[attValue1]}</span>
+        </div>
+        <div class="flex flex-col justify-center items-center">
+          <span class="text-[.6rem] font-medium">${attName2}</span>
+          <span class="text-[.63rem] font-bold ">${player[attValue2]}</span>
+        </div>
+        <div class="flex flex-col justify-center items-center">
+          <span class="text-[.6rem] font-medium">${attName3}</span>
+          <span class="text-[.63rem] font-bold ">${player[attValue3]}</span>
+        </div>
+        <div class="flex flex-col justify-center items-center">
+          <span class="text-[.6rem] font-medium">${attName4}</span>
+          <span class="text-[.63rem] font-bold">${player[attValue4]}</span>
+        </div>
+        <div class="flex flex-col justify-center items-center">
+          <span class="text-[.6rem] font-medium">${attName5}</span>
+          <span class="text-[.63rem] font-bold ">${player[attValue5]}</span>
+        </div>
+        <div class="flex flex-col justify-center items-center">
+          <span class="text-[.6rem] font-medium">${attName6}</span>
+          <span class="text-[.63rem] font-bold">${player[attValue6]}</span>
+        </div>
+    </div>`;
+
         newCard.addEventListener('click', () => {
 
             if (playersXI.includes(selectedPlace.name)) {
                 const playerIndex = playersXI.indexOf(selectedPlace.name);
                 playersXI.splice(playerIndex, 1);
             }
-            
+
             popUp.classList.add('hidden');
             selectedPlace.innerHTML = "";
             selectedPlace.classList.remove('holder-card');
             selectedPlace.classList.add('gold-card');
             selectedPlace.name = `${player.name}`
-            let attValue1, attValue2, attValue3, attValue4, attValue5, attValue6;
-            let attName1, attName2, attName3, attName4, attName5, attName6;
-            if (player.position != "GK") {
-                attValue1 = "pace";
-                attValue2 = "shooting";
-                attValue3 = "passing";
-                attValue4 = "dribbling";
-                attValue5 = "defending";
-                attValue6 = "physical";
-
-                attName1 = "PAC";
-                attName2 = "SHO";
-                attName3 = "PAS";
-                attName4 = "DRI";
-                attName5 = "DEF";
-                attName6 = "PHY";
-            } else {
-                attValue1 = "diving";
-                attValue2 = "handling";
-                attValue3 = "kicking";
-                attValue4 = "reflexes";
-                attValue5 = "positioning";
-                attValue6 = "speed";
-
-                attName1 = "DIV";
-                attName2 = "HAN";
-                attName3 = "KIC";
-                attName4 = "REF";
-                attName5 = "POS";
-                attName6 = "SPE";
-            }
             selectedPlace.innerHTML =
                 `<div class="flex w-[70%]">
                         <div class="flex flex-col items-center pt-4">
@@ -133,8 +160,8 @@ function showPopUpList(data) {
                           <span class="text-[.625rem] font-bold">${player[attValue6]}</span>
                         </div>
                     </div>
-                    <button class="remove-btn h-[0.9375rem] overflow-ellipsis text-[0.625rem] bg-red-600 text-[#e3b656] px-1 rounded-full font-bold absolute bottom-1">X</button>
-                 `
+                    <button class="remove-btn h-[0.9375rem] overflow-ellipsis text-[0.625rem] bg-red-700 text-white px-1 rounded-full font-bold absolute bottom-1">X</button>
+                 `;
             const removeBtn = selectedPlace.querySelector('.remove-btn');
             console.log(removeBtn);
             removeBtn.addEventListener('click', (event) => {
