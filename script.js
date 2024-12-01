16df3f9d4fe5b39b81bf0295cd862b2e76d9a206
MohamedHamouch <med10hamouch@gmail.com>
2024-12-01 03:52:15 +0100

    fetch data from home page

---
+++ script.js
@@ -0,0 +1,13 @@
+async function fetchPlayers() {
+    const response = await fetch('../assets/players.json');
+    const data = await response.json();
+    const players = data.players;
+    console.log(players);
+    data.players.forEach((player, index) => {
+          player.id = `${index}`;
+      });
+    localStorage.setItem('playersData', JSON.stringify(players));
+}
+window.onload = function () {
+    fetchPlayers();
+};
