1a1cc54dbcf8a20b80f76941375cda8d59bd9102
MohamedHamouch <med10hamouch@gmail.com>
2024-12-01 08:05:28 +0100

    fix remove from XI bug assosiate btn with id

--- JS/squad.js
+++ JS/squad.js
@@ -8,58 +8,58 @@ let selectedPlace;
 let playersXI = [];
 
 hidePopUpBtn.addEventListener('click', () => {
-    popUp.classList.toggle('hidden');
+  popUp.classList.toggle('hidden');
 });
 
 positions.forEach(positionPlace => {
-    positionPlace.addEventListener('click', () => {
-        popUp.classList.toggle('hidden');
-        filterPosition(positionPlace);
-        selectedPlace = positionPlace;
-    });
+  positionPlace.addEventListener('click', () => {
+    popUp.classList.toggle('hidden');
+    filterPosition(positionPlace);
+    selectedPlace = positionPlace;
+  });
 });
 
 function showPopUpList(data) {
-    popUpList.innerHTML = "";
-    data.forEach(player => {
+  popUpList.innerHTML = "";
+  data.forEach(player => {
 
 
-        let attValue1, attValue2, attValue3, attValue4, attValue5, attValue6;
-        let attName1, attName2, attName3, attName4, attName5, attName6;
-        if (player.position != "GK") {
-            attValue1 = "pace";
-            attValue2 = "shooting";
-            attValue3 = "passing";
-            attValue4 = "dribbling";
-            attValue5 = "defending";
-            attValue6 = "physical";
+    let attValue1, attValue2, attValue3, attValue4, attValue5, attValue6;
+    let attName1, attName2, attName3, attName4, attName5, attName6;
+    if (player.position != "GK") {
+      attValue1 = "pace";
+      attValue2 = "shooting";
+      attValue3 = "passing";
+      attValue4 = "dribbling";
+      attValue5 = "defending";
+      attValue6 = "physical";
 
-            attName1 = "PAC";
-            attName2 = "SHO";
-            attName3 = "PAS";
-            attName4 = "DRI";
-            attName5 = "DEF";
-            attName6 = "PHY";
-        } else {
-            attValue1 = "diving";
-            attValue2 = "handling";
-            attValue3 = "kicking";
-            attValue4 = "reflexes";
-            attValue5 = "positioning";
-            attValue6 = "speed";
+      attName1 = "PAC";
+      attName2 = "SHO";
+      attName3 = "PAS";
+      attName4 = "DRI";
+      attName5 = "DEF";
+      attName6 = "PHY";
+    } else {
+      attValue1 = "diving";
+      attValue2 = "handling";
+      attValue3 = "kicking";
+      attValue4 = "reflexes";
+      attValue5 = "positioning";
+      attValue6 = "speed";
 
-            attName1 = "DIV";
-            attName2 = "HAN";
-            attName3 = "KIC";
-            attName4 = "REF";
-            attName5 = "POS";
-            attName6 = "SPE";
-        }
+      attName1 = "DIV";
+      attName2 = "HAN";
+      attName3 = "KIC";
+      attName4 = "REF";
+      attName5 = "POS";
+      attName6 = "SPE";
+    }
 
-        const newCard = document.createElement('button');
-        newCard.className = 'text-center flex flex-col justify-center player gold-card h-[13.75rem] w-[10rem] items-center'
-        newCard.innerHTML =
-            `<div class="flex w-[90%]">
+    const newCard = document.createElement('button');
+    newCard.className = 'text-center flex flex-col justify-center player gold-card h-[13.75rem] w-[10rem] items-center'
+    newCard.innerHTML =
+      `<div class="flex w-[90%]">
         <div class="flex flex-col items-center pt-4">
             <span class="font-extrabold text-base leading-4">${player.rating}</span>
             <span class="font-bold text-xs">${player.position}</span>
@@ -98,20 +98,20 @@ function showPopUpList(data) {
         </div>
     </div>`;
 
-        newCard.addEventListener('click', () => {
+    newCard.addEventListener('click', () => {
 
-            if (playersXI.includes(selectedPlace.name)) {
-                const playerIndex = playersXI.indexOf(selectedPlace.name);
-                playersXI.splice(playerIndex, 1);
-            }
+      if (playersXI.includes(selectedPlace.name)) {
+        const playerIndex = playersXI.indexOf(selectedPlace.name);
+        playersXI.splice(playerIndex, 1);
+      }
 
-            popUp.classList.add('hidden');
-            selectedPlace.innerHTML = "";
-            selectedPlace.classList.remove('holder-card');
-            selectedPlace.classList.add('gold-card');
-            selectedPlace.name = `${player.id}`
-            selectedPlace.innerHTML =
-                `<div class="flex w-[70%]">
+      popUp.classList.add('hidden');
+      selectedPlace.innerHTML = "";
+      selectedPlace.classList.remove('holder-card');
+      selectedPlace.classList.add('gold-card');
+      selectedPlace.name = `${player.id}`
+      selectedPlace.innerHTML =
+        `<div class="flex w-[70%]">
                         <div class="flex flex-col items-center pt-4">
                             <span class="font-extrabold text-base leading-4">${player.rating}</span>
                             <span class="font-bold text-xs">${player.position}</span>
@@ -149,34 +149,61 @@ function showPopUpList(data) {
                           <span class="text-[.625rem] font-bold">${player[attValue6]}</span>
                         </div>
                     </div>
-                    <button class="remove-btn h-[0.9375rem] overflow-ellipsis text-[0.625rem] bg-red-700 text-white px-1 rounded-full font-bold absolute bottom-1">X</button>
+                    <button id="${player.id}" class="remove-btn h-[0.9375rem] overflow-ellipsis text-[0.625rem] bg-red-700 text-white px-1 rounded-full font-bold absolute bottom-1">X</button>
                  `;
-            const removeBtn = selectedPlace.querySelector('.remove-btn');
-            console.log(removeBtn);
-            removeBtn.addEventListener('click', (event) => {
-                event.stopPropagation();
-                let positionShort = selectedPlace.getAttribute('data-position');
-                console.log(positionShort);
-                selectedPlace.innerHTML = `Add ${positionShort}`;
-                selectedPlace.classList.remove('gold-card');
-                selectedPlace.classList.add('holder-card');
-                selectedPlace.name = "";
-                if (playersXI.includes(player.id)) {
-                    const playerIndex = playersXI.indexOf(player.id);
-                    playersXI.splice(playerIndex, 1);
-                }
-            });
+      const removeBtn = selectedPlace.querySelector('.remove-btn');
+      console.log(removeBtn);
+      removeBtn.addEventListener('click', (event) => {
+        event.stopPropagation();
+        let theID = removeBtn.id;
+        const place = document.querySelector(`[name="${theID}"]`);
+        console.log(place)
+        let positionShort = place.getAttribute('data-position');
+        console.log(positionShort);
+        place.innerHTML = `Add ${positionShort}`;
+        place.classList.remove('gold-card');
+        place.classList.add('holder-card');
+        place.name = "";
+        if (playersXI.includes(player.id)) {
+          const playerIndex = playersXI.indexOf(player.id);
+          playersXI.splice(playerIndex, 1);
+        }
+      });
 
-            playersXI.push(player.id);
-            // console.log(playersXI);
-        })
-        popUpList.appendChild(newCard);
-    });
+      playersXI.push(player.id);
+      // console.log(playersXI);
+    })
+    popUpList.appendChild(newCard);
+  });
 }
 
 function filterPosition(FilteringPositionPlace) {
-    let positionName = FilteringPositionPlace.getAttribute('data-position');
-    const filteredPlayers = playersData.filter(player => player.position === positionName && !playersXI.includes(player.id));
-    showPopUpList(filteredPlayers);
+  let positionName = FilteringPositionPlace.getAttribute('data-position');
+  const filteredPlayers = playersData.filter(player => player.position === positionName && !playersXI.includes(player.id));
+  showPopUpList(filteredPlayers);
 }
 
+// xiplayer = document.querySelectorAll(".players")
+// xiplayer.forEach(place => {
+//   place.addEventListener('hover', (event) => {
+//     console.log(place)
+//     event.stopPropagation();
+//     const removeBtn = place.querySelector('.remove-btn');
+//     removeBtn.addEventListener('click', (event) => {
+//       event.stopPropagation();
+//       // let theID = removeBtn.id;
+//       // const place = document.querySelector(`[name="${theID}"]`);
+//       // console.log(place)
+//       let positionShort = place.getAttribute('data-position');
+//       console.log(positionShort);
+//       place.innerHTML = `Add ${positionShort}`;
+//       place.classList.remove('gold-card');
+//       place.classList.add('holder-card');
+//       place.name = "";
+//       if (playersXI.includes(player.id)) {
+//         const playerIndex = playersXI.indexOf(player.id);
+//         playersXI.splice(playerIndex, 1);
+//       }
+//     })
+//   })
+// })
